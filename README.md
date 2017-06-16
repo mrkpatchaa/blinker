45cb737b0e99741619bd9462b0a0d0277ea4c342
Raymond Médédé KPATCHAA <kraymond70@gmail.com>
2017-06-16 08:40:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+##**Give your eyes a rest will ya!**
+
+Inspired by the 20 20 20 rule, this is a little reminder to look 20 feet away from your screen every 20 minutes. Keep your eyes healthy, reduce eye strain, prevent headaches and increase productivity.
+
+This is a simple application based on electron that will display a tray icon to help you configure how you want it to work.
+
+Inspired by :  http://www.blynker.com/#
+
+
+## Download
+Check the releases folder.
+
+## Dev setup
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/rmkpatchaa/blinker
+# Go into the repository
+cd blinker
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+
+## Resources for Learning Electron
+
+- [electron.atom.io/docs](http://electron.atom.io/docs) - all of Electron's documentation
+- [electron.atom.io/community/#boilerplates](http://electron.atom.io/community/#boilerplates) - sample starter apps created by the community
+- [electron/electron-quick-start](https://github.com/electron/electron-quick-start) - a very basic starter Electron app
+- [electron/simple-samples](https://github.com/electron/simple-samples) - small applications with ideas for taking them further
+- [electron/electron-api-demos](https://github.com/electron/electron-api-demos) - an Electron app that teaches you how to use Electron
+- [hokein/electron-sample-apps](https://github.com/hokein/electron-sample-apps) - small demo apps for the various Electron APIs
---
+++ main.js
@@ -0,0 +1,240 @@
+const electron = require('electron')
+const {app, Tray, Menu, BrowserWindow} = require('electron')
+const notifier = require('node-notifier')
+const AutoLaunch = require('auto-launch')
+const Store = require('electron-store');
+const path = require('path')
+
+const appAutoLauncher = new AutoLaunch({
+    name: 'Blinker',
+    isHidden: true,
+    mac: {
+        useLaunchAgent: true
+    }
+})
+const store = new Store({
+    defaults: {
+        config: {
+            launchOnStartup: true,
+            playSound: true
+        }
+    }
+})
+
+const config = store.get('config')
+
+const TIMER_LABEL_STOPPED = 'Start the timer'
+const TIMER_LABEL_STARTED = 'Stop the timer'
+
+// Our global objects
+let mainWindow
+let aboutWindow
+let timerStarted
+let launchOnStartup = config.launchOnStartup || true
+let playSound = config.playSound || true
+let timer
+const maxExpositionTime = 20 * 60 * 1000
+const breakTime = 20 * 1000
+
+let tray = null
+let contextMenu = null
+
+const launchApplication = () => {
+    // Setup the menubar with an icon
+    tray = new Tray(path.join(__dirname, 'icons/tray.png'))
+
+    contextMenu = Menu.buildFromTemplate([
+        {
+            label: TIMER_LABEL_STARTED,
+            type: 'checkbox',
+            sublabel: 'Click to toggle timer',
+            click () {
+                toggleTimer()
+            }
+        },
+        {type: 'separator'},
+        {
+            type: 'checkbox',
+            label: 'Launch the application on system startup',
+            sublabel: 'If this option is enabled, application will start on system startup',
+            checked: launchOnStartup,
+            click () {
+                toggleStartup()
+            }
+        },
+        {type: 'separator'},
+        {
+            type: 'checkbox',
+            label: 'Play sounds with notifications',
+            sublabel: 'If this option is enabled, a sound will be played on every notification',
+            checked: playSound,
+            click () {
+                toggleSound()
+            }
+        },
+        {type: 'separator'},
+        {
+            type: 'normal',
+            label: 'About Blinker',
+            click () {
+                toggleAboutWindow()
+            }
+        },
+        {type: 'separator'},
+        {
+            role: 'quit',
+            label: 'Quit application'
+        }
+    ])
+
+    tray.setToolTip('Click to see the magic.')
+
+    tray.setContextMenu(contextMenu)
+
+    // Start our timer
+    startTimer()
+
+    // Enable launch on startup by default
+    appAutoLauncher.isEnabled()
+        .then(function (isEnabled) {
+            if (isEnabled) {
+                return;
+            }
+            appAutoLauncher.enable()
+                .then(function () {
+                    store.set('config.launchOnStartup', true);
+                })
+        })
+        .catch(function (err) {
+        })
+
+    // Set the about window
+    aboutWindow = new BrowserWindow({
+        width: 400,
+        height: 350,
+        show: false,
+        frame: false,
+        resizable: false
+    })
+    aboutWindow.loadURL(`file://${path.join(__dirname, 'about.html')}`)
+    aboutWindow.on('blur', () => {
+        aboutWindow.hide()
+    })
+
+}
+
+const toggleStartup = () => {
+    if (launchOnStartup) {
+        appAutoLauncher.enable()
+            .then(function () {
+                store.set('config.launchOnStartup', true);
+                notifyLaunchChanged()
+            })
+            .catch(function (err) {
+            })
+    }
+    else {
+        appAutoLauncher.disable()
+            .then(function () {
+                store.set('config.launchOnStartup', false);
+                notifyLaunchChanged()
+            })
+            .catch(function (err) {
+            })
+    }
+}
+
+const notifyLaunchChanged = () => {
+    notifier.notify({
+        title: 'Startup status changed',
+        message: launchOnStartup ? 'You will have to launch the application every time you log in' : 'Gret! Application will launch on application startup',
+        icon: path.join(__dirname, 'icons/eye.png'),
+        contentImage: '',
+        sound: playSound
+    })
+    launchOnStartup = !launchOnStartup
+    contextMenu.items[2].checked = launchOnStartup
+    tray.setContextMenu(contextMenu)
+}
+
+const toggleTimer = () => {
+    if (timerStarted) {
+        stopTimer()
+        contextMenu.items[1].label = TIMER_LABEL_STOPPED
+    }
+    else {
+        startTimer()
+        contextMenu.items[1].label = TIMER_LABEL_STARTED
+    }
+    tray.setContextMenu(contextMenu)
+}
+
+const toggleSound = () => {
+    playSound = !playSound
+    store.set('config.playSound', playSound);
+}
+
+const startTimer = () => {
+    timerStarted = true
+    timer = setTimeout(function () {
+        takeABreak()
+    }, maxExpositionTime);
+}
+
+const stopTimer = () => {
+    timerStarted = false
+    clearTimeout(timer)
+}
+
+const takeABreak = () => {
+    stopTimer()
+
+    notifier.notify({
+        title: 'Take a break !',
+        message: 'Great! Application will launch on application startup',
+        icon: path.join(__dirname, 'icons/eye.png'),
+        contentImage: '',
+        sound: playSound
+    })
+
+    setTimeout(function () {
+        notifier.notify({
+            title: 'Go back to work !',
+            message: 'Great! Application will launch on application startup',
+            icon: path.join(__dirname, 'icons/eye.png'),
+            contentImage: '',
+            sound: playSound
+        })
+        startTimer()
+    }, breakTime)
+}
+
+const toggleAboutWindow = () => {
+    if (aboutWindow.isVisible()) {
+        aboutWindow.hide()
+    } else {
+        aboutWindow.show()
+    }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', launchApplication)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createTray()
+    }
+})
